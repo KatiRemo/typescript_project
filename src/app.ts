c8eb2a3c62de4084b71ec0c20bd43348cc0a2ecd
BC-macair028 <bc-macair028@BC-macair028s-MacBook-Air.local>
2022-01-28 11:47:05 +0200

    update ts

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@
 
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
-    const adjDescriptor = PropertyDescriptor = {
+    const adjDescriptor: PropertyDescriptor = {
         configurable: true,
         get() {
             const boundFn = originalMethod.bind(this);
